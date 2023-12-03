e38febe092fe411151092538b767c21f7c698a9f
olekwa <johnolekwa@gmail.com>
2023-12-02 23:55:10 -0800

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-# This will get the byte-size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This would get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
